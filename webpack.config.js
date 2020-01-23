d0c70a6462bba1f42eb61d0fd312dd8df8b57d0f
Steven Hobson-Campbell <xapphire13@live.com>
2020-01-22 23:26:04 -0800

    rename some dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,49 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const nodeExternals = require("webpack-node-externals");
+const nodeExternals = require('webpack-node-externals');
 
 const baseConfig = {
-  devtool: "source-map",
-  mode: "development",
+  devtool: 'source-map',
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js']
   },
-  plugins: [new webpack.ProgressPlugin()],
-}
+  plugins: [new webpack.ProgressPlugin()]
+};
 
 const clientConfig = {
   ...baseConfig,
 
-  entry: "./src/client/index.tsx",
-  target: "electron-renderer",
+  entry: './src/renderer/index.tsx',
+  target: 'electron-renderer',
   output: {
-    filename: 'client.js',
+    filename: 'renderer.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "./"
+    publicPath: './'
   },
-  plugins: [
-    ...baseConfig.plugins,
-    new HtmlWebpackPlugin()
-  ]
+  plugins: [...baseConfig.plugins, new HtmlWebpackPlugin()]
 };
 
 const serverConfig = {
   ...baseConfig,
 
-  entry: "./src/server/index.ts",
-  target: "electron-main",
+  entry: './src/main/index.ts',
+  target: 'electron-main',
   output: {
-    filename: 'server.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
   externals: [nodeExternals()]
-}
+};
 
 module.exports = [clientConfig, serverConfig];
